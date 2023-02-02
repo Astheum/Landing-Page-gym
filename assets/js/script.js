d25ddca3e7538783b556bf2eafdb18bc30c25b07
Astheum <119074740+Astheum@users.noreply.github.com>
2023-02-02 16:11:03 -0300

    atualização do projeto
    Atualizei a pasta onde ficava os arquivos de estilo e script e terminei o menu

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function clickMenu(){
+    if (nav.style.display == 'block'){
+        nav.style.display = 'none'
+    }
+    
+    else {
+        nav.style.display = 'block'
+    }
+}
